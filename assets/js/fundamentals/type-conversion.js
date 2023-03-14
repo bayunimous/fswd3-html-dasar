8984a8c86e51a113fcb0d218e6d4f3d1e420ad70
bayunimous <jigs4wcode@gmail.com>
2023-03-14 16:00:12 +0800

    [FEAT] Menambahkan File Fundamentals - Type Conversion

---
+++ assets/js/fundamentals/type-conversion.js
@@ -0,0 +1,19 @@
+//type conversion string
+// value = String(value); // now value is a string "true"
+// alert(typeof value); // string
+
+// // type conversion number
+// alert("6" / "2"); // 3, strings are converted to numbers
+
+// let str = "123";
+// alert(typeof str); // string
+
+// let num = Number(str); // becomes a number 123
+
+// alert(typeof num); // number
+
+alert(Boolean(1)); //true
+alert(Boolean(0)); //false
+
+alert(Boolean("hello")); //true
+alert(Boolean("")); //false
