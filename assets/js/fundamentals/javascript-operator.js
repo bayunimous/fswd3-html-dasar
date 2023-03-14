1cb7a5da85f28ea9df171eb42dae10a333069e15
bayunimous <jigs4wcode@gmail.com>
2023-03-14 18:33:56 +0800

    [FEAT] Menambahkan File Fundamentals - Javascript Operator

---
+++ assets/js/fundamentals/javascript-operator.js
@@ -0,0 +1,49 @@
+//Arithmetic Operator
+// let a = 10;
+// let b = 3;
+
+// console.log(a + b); //13
+// console.log(a - b); //7
+// console.log(a * b); //30
+// console.log(a / b); //3.3333333333333335
+// console.log(a % b); //1
+// console.log(a ** b); //1000
+
+//Assignment Operator
+// let a = 2;
+// console.log(a); //2
+
+// a += 5;
+// console.log(a); //7
+
+//Comparison Operator
+// let a = 4;
+// let b = 5;
+
+// console.log(a == b);
+// console.log(a < b);
+// console.log(a > b);
+// console.log(a != b);
+
+//Logical Operator
+// let a = true;
+// let b = false;
+
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);
+
+//Bitwise Operator
+// let a = 00111100;
+// let b = 00001101;
+
+// console.log(a & b); //12
+// console.log(a | b);
+// console.log(a ^ b);
+// console.log(~a);
+
+//Ternary Operator
+let a = 1;
+let b = 1;
+
+console.log(a == b ? "YES" : "NO");
