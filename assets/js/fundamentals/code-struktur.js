5dfe2910a5bc46fc57f40be7de00c2ed5c8ddc48
bayunimous <jigs4wcode@gmail.com>
2023-03-14 15:20:55 +0800

    [FEAT] Menambahkan File Fundamentals - Code Structure

---
+++ assets/js/fundamentals/code-struktur.js
@@ -0,0 +1,16 @@
+//statement
+console.log("Hello World!");
+alert("Hello World!");
+
+console.log("Hello World!");
+alert("Hello World!");
+
+//Komentar satu baris
+
+/* 
+Komentar
+multi
+baris
+*/
+
+// Path: assets\js\fundamentals\code-struktur.js
