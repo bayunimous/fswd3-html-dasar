9d5af9a41148e960829cd35fa6de1ab0a516832e
bayunimous <jigs4wcode@gmail.com>
2023-03-14 19:07:04 +0800

    [FEAT] Menambahkan File Fundamentals - Javascript Loop

---
+++ assets/js/fundamentals/javascript-loop.js
@@ -0,0 +1,36 @@
+//FOR
+// for (let i = 0; i < 5; i++) {
+//   console.log(`Iterasi ke ${i}`);
+// }
+
+//DO WHILE
+// let i = 0;
+
+// do {
+//   console.log(`Iterasi ke ${i}`);
+//   i++;
+// } while (i < 5);
+
+//WHILE
+// let i = 0;
+
+// while (i < 5) {
+//   console.log(`Iterasi ke ${i}`);
+//   i++;
+// }
+
+//LOOP CONTROL - BREAK
+// for (let i = 0; i < 5; i++) {
+//   if (i === 3) {
+//     break;
+//   }
+//   console.log(`Iterasi ke ${i}`);
+// }
+
+//LOOP CONTROL - CONTINUE
+for (let i = 0; i < 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(`Iterasi ke ${i}`);
+}
