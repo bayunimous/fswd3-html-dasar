643f631dfdca07ccf0d8bc448acca8d7e053fb90
bayunimous <jigs4wcode@gmail.com>
2023-03-14 15:45:13 +0800

    [FEAT] Menambahkan File Fundamentals - Data Types

---
+++ assets/js/fundamentals/data-types.js
@@ -0,0 +1,37 @@
+//BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+
+//String
+const nama1 = "Arkatama";
+const nama2 = "Arkatama";
+const nama3 = `Arkatama`;
+
+//Boolean
+let isLoggedIn = true;
+let isAdmin = false;
+
+let isGreater = 10 > 5;
+let isLess = 10 < 5;
+
+//Null
+let girlFriend = null;
+
+//Undefined
+let name;
+console.log("name");
+
+//Symbol
+let employee = Symbol("Joko");
+console.log(employee);
+console.log(employee.description);
+
+//Object
+// let employee = {
+//   name: "John",
+//   age: 30,
+//   job: "Web Developer",
+//   isMarred: false,
+//   hobbies: ["Sports", "Cooking"],
+// };
+
+// console.log(`His name is ${employee.name} and he is ${employee.age} years old`);
