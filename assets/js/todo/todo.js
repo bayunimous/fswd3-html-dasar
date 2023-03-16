caba67822f3bca7024e117b3f110d8da3f39627f
bayunimous <jigs4wcode@gmail.com>
2023-03-16 14:05:18 +0800

    [FIX]: Menambahkan styling css pada todo.css

---
+++ assets/js/todo/todo.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+const input = document.querySelector("form input");
+const todos = document.querySelector(".todos");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const text = input.value.trim();
+  if (text !== "") {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    span.textContent = text;
+    button.textContent = "Delete";
+    li.appendChild(span);
+    li.appendChild(button);
+    todos.appendChild(li);
+    input.value = "";
+  }
+});
+
+todos.addEventListener("click", function (event) {
+  if (event.target.tagName === "BUTTON") {
+    event.target.parentNode.remove();
+  }
+});
